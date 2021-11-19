ee8f34b77638e48e5c2afc9594cdb7526325384c
Grigorii <gregsh9.5@gmail.com>
2021-11-19 10:48:01 +0300

    improve 862

--- monotonic-queue/ShortestSubarrayWithSumAtLeastK.java
+++ monotonic-queue/ShortestSubarrayWithSumAtLeastK.java
@@ -8,7 +8,8 @@ import java.util.Deque;
  *
  * Task.
  *
- * Return the length of the shortest, non-empty, contiguous subarray of A with sum at least K.
+ * Return the length of the shortest, non-empty, contiguous subarray of A with
+ * sum at least K.
  *
  * If there is no non-empty subarray with sum at least K, return -1.
  *
@@ -16,66 +17,57 @@ import java.util.Deque;
  *
  * Source: Leetcode
  */
-public class ShortestSubarrayWithSumAtLeastK
-{
-    class Solution
-    {
-        public int shortestSubarray(int[] A, int K)
-        {
-            IMQ q = new IMQ(K);
-            q.push(new Item(0, -1));
-            for (int i = 0; i < A.length; i++)
-            {
-                // rewriting array with prefix sums
-                A[i] = i > 0 ? A[i] + A[i - 1] : A[0];
-                q.push(new Item(A[i], i));
-            }
+public class ShortestSubarrayWithSumAtLeastK {
+  class Solution {
+    public int shortestSubarray(int[] A, int K) {
+      IMQ q = new IMQ(K);
+      q.push(new Item(0, -1));
+      long cur = 0, prev = 0;
 
-            return q.min != Integer.MAX_VALUE ? q.min : -1;
-        }
+      for (int i = 0; i < A.length; i++) {
+        cur = prev + A[i];
+        prev = cur;
+        q.push(new Item(cur, i));
+      }
 
-        // increasing monotone queue
-        private class IMQ
-        {
-            private Deque<Item> q = new ArrayDeque<>();
-            private int min = Integer.MAX_VALUE;
-            private int K;
-
-            public IMQ(int K)
-            {
-                this.K = K;
-            }
-
-            private void push(Item newItem)
-            {
-                while (!q.isEmpty() && newItem.val < q.peekLast().val)
-                {
-                    q.removeLast();
-                }
-
-                // sliding window (minimum) part - bigger subarray satisfies our condition
-                // that's why we can short it (move left pointer)
-                // prefixSum[j] - prefixSum[i] >= K => prefixSum[i] <= prefixSum[j] - K
-                // while pre[r] - pre[l] >= k
-                while (!q.isEmpty() && newItem.val - q.peekFirst().val >= K)
-                {
-                    min = Math.min(min, newItem.ind - q.peekFirst().ind);
-                    q.removeFirst();
-                }
-
-                q.addLast(newItem);
-            }
-        }
-
-        private class Item
-        {
-            int val, ind;
-
-            Item(int val, int ind)
-            {
-                this.val = val;
-                this.ind = ind;
-            }
-        }
+      return q.min != Integer.MAX_VALUE ? q.min : -1;
     }
+
+    // increasing monotone queue
+    private class IMQ {
+      private Deque<Item> q = new ArrayDeque<>();
+      private int K, min = Integer.MAX_VALUE;
+
+      public IMQ(int K) {
+        this.K = K;
+      }
+
+      private void push(Item newItem) {
+        while (!q.isEmpty() && newItem.val < q.peekLast().val) {
+          q.removeLast();
+        }
+
+        // sliding window (minimum) part - bigger subarray satisfies our condition
+        // that's why we can short it (move left pointer)
+        // prefixSum[j] - prefixSum[i] >= K => prefixSum[i] <= prefixSum[j] - K
+        // while pre[r] - pre[l] >= k
+        while (!q.isEmpty() && newItem.val - q.peekFirst().val >= K) {
+          min = Math.min(min, newItem.ind - q.peekFirst().ind);
+          q.removeFirst();
+        }
+
+        q.addLast(newItem);
+      }
+    }
+
+    private class Item {
+      long val;
+      int ind;
+
+      Item(long val, int ind) {
+        this.val = val;
+        this.ind = ind;
+      }
+    }
+  }
 }
